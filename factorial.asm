da9caac985700edc0c8bca5d6972993a3196dac1
ntIgnant <ignanttt@gmail.com>
2025-02-03 19:09:26 +0100

    UPDATED | factorial prob not finished

--- factorial.asm
+++ factorial.asm
@@ -15,6 +15,16 @@ main:
 
 
 factorial:
+    addi $sp, $sp, -4   # allocate 4 bytes in stack
+    sw $ra, 0($sp)  # save ra of main into those 4 bytes
+
+    # create a copy of the value of s0
+    li $s1, $s0
+
+    li $t0, 1   # create a 'counter'
+    
+    slt %a0, $t0, $s0   # it's like a0 = 1 IF t0 < s0, otherwise a0 = 0 
+    
     
 
 
