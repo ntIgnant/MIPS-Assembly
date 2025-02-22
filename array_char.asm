6547c3411861752957807c70b6f95b6d345b6750
ntIgnant_G14 <ignanttt@gmail.com>
2025-02-22 15:54:50 +0100

    UPDATED | character array file fixed

--- array_char.asm
+++ array_char.asm
@@ -1,54 +1,61 @@
 .data
-
-    # -Local Variables for the Game-
-    # I'll probably run out of registers, but for now it's ok, othwerise I'll just harcode the load-immediate values
-    hash_char: .asciiz "#"
+    # Characters
+    hash_char: .asciiz "#"  
     newline_char: .asciiz "\n"
-    space_char: .asciiz " "
-
-    # save space for the array (7x7 bytes)
-    grid: .space 49 # save 49 bytes of memory for the array/grid
+    
+    # Grid (7x7 bytes)
+    grid: .space 49  
 
 .text
 .globl main
 
-
 main:
-    # load the base address of the grid into a temp-register
-    la $t0, grid    # now, t0 has the base address of the array
+    # Load the base address of the grid
+    la $t0, grid  # $t0 holds base address
 
-    li $t1, 0   # load '0' in temp-type register | this will work as the array index
-    li $t2, 7   # load '7' in  temp-type register | this will work as the total number of rows (7) and the number of values per row
+    li $t1, 0     # Index counter
+    li $t2, 7     # Number of characters per row
 
-    li $t3, hash_char   # load the "#" character into t3 temp-register
-
+    # Load '#' character (byte)
+    li $t3, '#'  
 
 build_grid_row:
-    sw $t3, 0($t1)  # store the value '#' inside the iterable location of the array memory address
-
-    addi $t1, $t1, 1    # add +1 to t1 (array index) to go to the next cell at memory level (+1 byte)
+    # Store '#' in the current cell (1 byte)
+    sb $t3, 0($t0)  
     
-    bne $t1, $t2, build_grid_row    # while t1(array index) not equal to t2(number of characters per row), jump to build_grid_row
-    #else:
+    # Move to the next cell
+    addi $t0, $t0, 1  
+    addi $t1, $t1, 1  
+    
+    # Loop until 7 characters are stored
+    blt $t1, $t2, build_grid_row  
 
-    # reset the value of the index to 0 for the print function to start from 0 index
-    li $t1, 0   # t1(array index) re-set to 0
-
-    j print_row # jump to print_row
+    # Reset index for printing
+    li $t1, 0  
+    la $t0, grid  # Reset base address
 
 print_row:
-    
-    lw $t4, 0($t1)  # load the value of the array address at position (t1)
-    
-    move $a0, $t4   # move the value of t4('#') into an argument-type register
-    li $v0, 1   # load '1' into return-type register to print out the value of the array
-    sysycall
+    # Load character from grid
+    lb $a0, 0($t0)  
 
-    bne $t1, $t2, print_row # while t1(array index) not equal to t2(number of characters per row), jump to print_row
-    #else:
+    # Print character
+    li $v0, 11  
+    syscall  
 
-    j exit  # jumpt to exit function
+    # Move to the next byte
+    addi $t0, $t0, 1  
+    addi $t1, $t1, 1  
+    
+    # Loop until we print 7 characters
+    blt $t1, $t2, print_row  
+
+    # Print newline
+    li $a0, '\n'  
+    li $v0, 11  
+    syscall  
+
+    j exit  
 
 exit:
-    li $v0, 10
-    syscall
+    li $v0, 10  
+    syscall  
