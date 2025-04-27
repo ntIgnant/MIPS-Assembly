4e5ea2b9cd2c86f9bd4a149d204461c40fd8f708
ntIgnant <ignanttt@gmail.com>
2025-04-27 23:16:09 +0200

    UPDATED | character array asm basic

--- array_char.asm
+++ array_char.asm
@@ -16,13 +16,16 @@ main:
     li $t1, 0     # Index counter
     li $t2, 7     # Number of characters per row
 
+    li $t4, 0   # row counter
+
     # Load '#' character (byte)
     li $t3, '#'  
 
-build_grid_row:
-    # Store '#' in the current cell (1 byte)
+build_grid:
+    # Store '#' in the current cell of the array
     sb $t3, 0($t0)  
     
+build_grid_row:
     # Move to the next cell
     addi $t0, $t0, 1  
     addi $t1, $t1, 1  
@@ -30,9 +33,20 @@ build_grid_row:
     # Loop until 7 characters are stored
     blt $t1, $t2, build_grid_row  
 
-    # Reset index for printing
-    li $t1, 0  
-    la $t0, grid  # Reset base address
+    # add +1 to the row counter (should be 7 rows at the end)
+    addi $t4, $t4 1
+
+    # reset the index for the newxt row
+    li $t1, 0
+
+    # berify if there are rows missing
+    bneq $t4, $t2, build_grid_row   # while t4(row counter) not equal to t2(total rows), jump to build_grid_row function
+    # else:
+    
+    # if the program reaches this point, it means that all the 7 rows had been filled (7x7 grid)
+    # reset the base address adn the index of the array for the printing 
+    li $t0, 0
+    la $t0, grid    # load the base address of the array into the temp-register t0
 
 print_row:
     # Load character from grid
@@ -52,7 +66,15 @@ print_row:
     # Print newline
     li $a0, '\n'  
     li $v0, 11  
-    syscall  
+    syscall
+
+    # at this point, one row should have been printed
+
+    addi $t4, $t4, 1    # add +1 to temp-register t4(counter for number of rows)
+
+    # check if there are rows missing
+    bneq $t4, $t2, print_row    # while t4(row counter) not equal to t2(total rows), jump to print_row function
+    #else:
 
     j exit  
 
