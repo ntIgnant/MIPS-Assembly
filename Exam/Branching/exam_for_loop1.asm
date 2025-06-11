64565d1c1e95dea98f5f0e130adcfd717948df34
ntIgnant <ignanttt@gmail.com>
2025-06-11 23:17:35 +0200

    UPDATED | for loop done in mips asm | exam prep

--- Exam/Branching/exam_for_loop1.asm
+++ Exam/Branching/exam_for_loop1.asm
@@ -11,12 +11,27 @@ num1: .word 10 # .word variables are 4 bytes each (almost sure ngl)
 la $s0, msg # load the address because it is like an 'array' actually
 lw $s1, num1 # load the word (number) into register s1
 
+li $t0, 10
+li $t1, 100
+
 jal loop
 j exit
 
 loop:
 
+# print the number (10, 20, 30, ...)
+
+move $a0, $t0
+li $v0, 1 # load 1 t the return type register to print an int
+syscall
+
+addi $t0, $t0, 10 # it will be adding up 10, so 10, 20, 30, ... till 100
+
+blt $t0, $t1, loop # wile t0 < t1 (100), jump to loop
+#else:
+
+jr $ra # jump back to the register return address (caller main)
 
 exit:
-
-
+li $v0, 10 # load 10 to the return type regsyer to syscall 10 (terminate program)
+syscall
