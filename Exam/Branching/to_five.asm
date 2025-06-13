6bb60be8a3c3c56a018052989a0a69846448de4f
ntIgnant <ignanttt@gmail.com>
2025-06-13 23:59:28 +0200

    UPDATED | for loop with mips asm

--- Exam/Branching/to_five.asm
+++ Exam/Branching/to_five.asm
@@ -1,4 +1,5 @@
 .data #space for the local static variables
+	msg: .asciiz "DONE!\n"
 
 .ktext # space for the code tthat will be executed in kernel space
 
@@ -6,4 +7,18 @@
 .text # space for the code that will be executed (functionality, flow, etc)
 
 main:
+	# laod the static variables into registers
+	la $s0, msg # load the address of the message into the store type registers s0
+
+	# set up the register values before enter to the loop
+	li $t0, 1 # load the immediate value '1' into the register s0
 	
+
+	jal loop # jump and link to the label 'loop'
+	j exit # jump to 'exit'
+
+loop:
+
+exit:
+	li $v0, 10 # load '10' into the return type register the value 10 for systcall 10 (exit)
+	syscall
