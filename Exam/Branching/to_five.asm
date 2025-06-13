bc26a93b4c6992581285218ce0428717108a11fa
ntIgnant <ignanttt@gmail.com>
2025-06-14 00:25:40 +0200

    UPDATED | for loop finished | exam prep

--- Exam/Branching/to_five.asm
+++ Exam/Branching/to_five.asm
@@ -12,12 +12,35 @@ main:
 
 	# set up the register values before enter to the loop
 	li $t0, 1 # load the immediate value '1' into the register s0
-	
+	li $t1, 6 # load immediate value | this will be the max value to compare the beq to
 
 	jal loop # jump and link to the label 'loop'
 	j exit # jump to 'exit'
 
 loop:
+	# load the value of the current number (stored in t0 register)
+	move $a0, $t0 # move the value of t0 into the the argument type register
+	
+	# load 1 to the regurn type register for syscall 1 (print int)
+	li $v0, 1
+	syscall
+
+	# add '+1' to the result of t0
+	addi $t0, $t0, 1 # add immediate '1' to the register t0
+	
+	# verify the value of t0, if it is equal to '6', then brek the loop (because 1 ... 5 already printed)
+	blt $t0, $t1, loop # while t0 < t1, jump to loop
+	#else:
+
+	# print the message
+	move $a0, $s0
+	
+	# load 4 to the return type register for syscall 4
+	li $v0, 4
+	syscall
+
+	# jump bac to the caller 'main'
+	jr $ra # jump to the return address 
 
 exit:
 	li $v0, 10 # load '10' into the return type register the value 10 for systcall 10 (exit)
