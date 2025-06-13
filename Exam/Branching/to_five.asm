0c137cee727f9c97710e944d16b6416dfa630db7
ntIgnant <ignanttt@gmail.com>
2025-06-13 23:49:43 +0200

    UPDATED | for loop to count till five done with mips asm | exam prep

---
+++ Exam/Branching/to_five.asm
@@ -0,0 +1,9 @@
+.data #space for the local static variables
+
+.ktext # space for the code tthat will be executed in kernel space
+
+.globl main # space for 'globaly visible' functions/lables, used when working with multiple files (link needed)
+.text # space for the code that will be executed (functionality, flow, etc)
+
+main:
+	
